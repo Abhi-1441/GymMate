61334db2f547e90abf3fbce9c2170090e6a13955
Abhi-1441 <pakhmodeabhishek1441@gmail.com>
2024-09-18 00:39:25 +0530

    configure for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// base: '/GymMate/',
-//   define: {
-//     'process.env': {},
-//   },
-//   build: {
-//     rollupOptions: {
-//       external: ['@react-oauth/google'],
-//     },
-//   },
-// })
-
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GymMate/',
-  // build: {
-  //   rollupOptions: {
-  //     external: ['@react-oauth/google'], // Add the module to the external list
-  //     output: {
-  //       globals: {
-  //         '@react-oauth/google': 'ReactOAuthGoogle',
-  //       },
-  //     },
-  //   },
-  // },
-});
+  define: {
+    'process.env': {},
+  },
+})
