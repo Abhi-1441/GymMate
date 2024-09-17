6f01a776f8f191d718623c804319ed8a799810ce
Abhi-1441 <pakhmodeabhishek1441@gmail.com>
2024-09-18 00:13:25 +0530

    Change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/GymMate/',
+//   define: {
+//     'process.env': {},
+//   },
+//   build: {
+//     rollupOptions: {
+//       external: ['@react-oauth/google'],
+//     },
+//   },
+// })
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/GymMate/',
-  define: {
-    'process.env': {},
-  },
   build: {
     rollupOptions: {
-      external: ['@react-oauth/google'],
+      external: ['@react-oauth/google'], // Add the module to the external list
+      output: {
+        globals: {
+          '@react-oauth/google': 'ReactOAuthGoogle',
+        },
+      },
     },
   },
-})
+});
